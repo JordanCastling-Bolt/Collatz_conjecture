a1303390db2735eec8a93082f1d47c0ed392a0be
Jordan Castling-Bolt <jordancastlingbolt@gmail.com>
2023-08-30 14:08:21 +0200

    collatz_conjecture

---
+++ Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace StockTicker
+{
+    partial class Form1
+    {
+   
+        private System.ComponentModel.IContainer components = null;
+   protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 
+
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnMH50 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnClearOutput = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.ListSize = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(23)))), ((int)(((byte)(58)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.Location = new System.Drawing.Point(26, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 13);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btnMH50
+            // 
+            this.btnMH50.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(23)))), ((int)(((byte)(58)))));
+            this.btnMH50.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnMH50.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnMH50.Location = new System.Drawing.Point(26, 72);
+            this.btnMH50.Name = "btnMH50";
+            this.btnMH50.Size = new System.Drawing.Size(77, 23);
+            this.btnMH50.TabIndex = 3;
+            this.btnMH50.Text = "MH 50";
+            this.btnMH50.UseVisualStyleBackColor = false;
+            this.btnMH50.Click += new System.EventHandler(this.btnMH50_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(23)))), ((int)(((byte)(58)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBox2.Location = new System.Drawing.Point(26, 101);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(181, 444);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(23, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Insert a number greater than zero";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5;
+            // 
+            // btnClearOutput
+            // 
+            this.btnClearOutput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(23)))), ((int)(((byte)(58)))));
+            this.btnClearOutput.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnClearOutput.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnClearOutput.Location = new System.Drawing.Point(26, 580);
+            this.btnClearOutput.Name = "btnClearOutput";
+            this.btnClearOutput.Size = new System.Drawing.Size(98, 23);
+            this.btnClearOutput.TabIndex = 7;
+            this.btnClearOutput.Text = "Clear Output";
+            this.btnClearOutput.UseVisualStyleBackColor = false;
+            this.btnClearOutput.Click += new System.EventHandler(this.btnClearOutput_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnStop.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnStop.Location = new System.Drawing.Point(130, 580);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(77, 23);
+            this.btnStop.TabIndex = 8;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 5;
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.chart1.BorderlineColor = System.Drawing.Color.WhiteSmoke;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(213, 48);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Excel;
+            this.chart1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "MH 50";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(905, 572);
+            this.chart1.TabIndex = 10;
+            this.chart1.Text = "Graph";
+            // 
+            // ListSize
+            // 
+            this.ListSize.Location = new System.Drawing.Point(26, 552);
+            this.ListSize.Name = "ListSize";
+            this.ListSize.Size = new System.Drawing.Size(181, 20);
+            this.ListSize.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(3)))), ((int)(((byte)(38)))));
+            this.ClientSize = new System.Drawing.Size(1201, 632);
+            this.Controls.Add(this.ListSize);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnClearOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnMH50);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Stock Ticker";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnMH50;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnClearOutput;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox ListSize;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Threading;
+
+namespace StockTicker
+{
+    public partial class Form1 : Form
+    {
+        Thread thread;
+        int listCount = 0; // New variable to count the list items
+
+        public Form1()
+        {
+            InitializeComponent();
+            chart1.Series["MH 50"].ChartType = SeriesChartType.Line;
+        }
+
+        private void btnMH50_Click(object sender, EventArgs e)
+        {
+            if (Int32.TryParse(textBox1.Text, out int n) && n > 0)
+            {
+                InitializeUI();
+                thread = new Thread(() => CalculateCollatz(n));
+                thread.Start();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a number greater than zero.");
+            }
+        }
+
+        private void CalculateCollatz(int n)
+        {
+            listCount = 0; // Reset list count
+
+            while (n > 1)
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    chart1.Series["MH 50"].Points.AddY(n);
+                    textBox2.AppendText(n.ToString() + Environment.NewLine);
+                });
+
+                listCount++; // Increment list count
+
+                if (n % 2 == 0)
+                {
+                    n /= 2;
+                }
+                else
+                {
+                    n = 3 * n + 1;
+                }
+            }
+
+            this.Invoke((MethodInvoker)delegate
+            {
+                chart1.Series["MH 50"].Points.AddY(1);
+                textBox2.AppendText("1" + Environment.NewLine);
+                ListSize.Text = listCount.ToString(); // Display the list count in the text box
+            });
+        }
+
+        private void btnClearOutput_Click(object sender, EventArgs e)
+        {
+            InitializeUI();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (thread != null && thread.IsAlive)
+            {
+                thread.Abort();
+                Close();
+            }
+            Close();
+        }
+
+        private void InitializeUI()
+        {
+            textBox2.Text = "";
+            chart1.Series["MH 50"].Points.Clear();
+            ListSize.Text = "0"; // Reset list count in the UI
+            listCount = 0; // Reset list count
+            if (thread != null && thread.IsAlive)
+            {
+                thread.Abort();
+            }
+        }
+    }
+}
